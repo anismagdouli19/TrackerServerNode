3ed0ec77cf8b3f9717d27955a8eb163648b028c7
Tapio Vihanta <ippe@lakka.kapsi.fi>
2015-12-21 17:32:57 +0200

    changed server to really use imei from device

--- src/tk102.js
+++ src/tk102.js
@@ -32,104 +32,117 @@ var specs = [
   //(027042699595BP00000027042699595HSO)
   //(027042699595BR00150809A6637.3671N02539.1402E004.0081628210.4800000000L00000000)
   function(raw) {
-    var result = null
-    try {
-        var raw = raw.trim()
-        var str = raw;
-        imei = str.match(/\(([0-9]*)BR/)[1];
-        date = str.match(/BR([0-9]{8})[A|V]/)[1];
-        time = str.match(/\E[0-9]{3,4}\..([0-9]{6})/)[1];
-        gpsData = str.match(/BR[0-9]{8}(A|V)/)[1];
-        lat = str.match(/[A|V]([0-9]{4,5})\.([0-9]{4})N/);
-        latDegMin = lat[1];
-        latSec = lat[2];
-        lon = str.match(/N([0-9]{4,5})\.([0-9]{4})E/);
-        lonDegMin = lon[1];
-        lonSec = lon[2];
-        speed = str.match(/E([0-9]{3}\.[0-9]{1})./);
-        speed = parseFloat(speed[1]);
-        var gpsdate = date.replace( /[0-9]{2}([0-9]{2})([0-9]{2})([0-9]{2})/, function( match, year, month, day ) {
-          return day +'.'+ month +'.'+ year
-        });
-        var gpstime = time.replace( /([0-9]{2})([0-9]{2})([0-9]{2})/, function( match, hour, minute, second ) {
-          return hour +':'+ minute +':'+ second
-        });
-        result = {
-          'raw': raw,
-          'datetime': new Date().toISOString(),
-        //  'phone': str[1],
-          'gps': {
-            'date': gpsdate,
-            'time': gpstime,
-            //'signal': str[15] == 'F' ? 'full' : 'low',
-            'fix': gpsData == 'A' ? 'active' : 'invalid'
-          },
-          'geo': {
-            'latitude': tk102.fixGeo( latDegMin, latSec ),
-            'longitude': tk102.fixGeo( lonDegMin, lonSec ),
-            'speed': speed
-            //'bearing': parseInt( str[10] )
-          },
-        /*  'speed': {
-            'knots': Math.round( str[9] * 1000 ) / 1000,
-            'kmh': Math.round( str[9] * 1.852 * 1000 ) / 1000,
-            'mph': Math.round( str[9] * 1.151 * 1000 ) / 1000
-          },*/
-          'imei': imei.trim()
+    var rawSplit = raw.split(')');
+    for(var i = 0, len = rawSplit.length; i<len; i++){
+        if(rawSplit[i] != ""){
+            var result = null
+            try {
+                var raw = raw.trim()
+                var str = raw;
+                imei = str.match(/\(([0-9]*)BR/)[1];
+                date = str.match(/BR([0-9]{8})[A|V]/)[1];
+                time = str.match(/\E[0-9]{3,4}\..([0-9]{6})/)[1];
+                gpsData = str.match(/BR[0-9]{8}(A|V)/)[1];
+                lat = str.match(/[A|V]([0-9]{4,5})\.([0-9]{4})N/);
+                latDegMin = lat[1];
+                latSec = lat[2];
+                lon = str.match(/N([0-9]{4,5})\.([0-9]{4})E/);
+                lonDegMin = lon[1];
+                lonSec = lon[2];
+                speed = str.match(/E([0-9]{3}\.[0-9]{1})./);
+                speed = parseFloat(speed[1]);
+                var gpsdate = date.replace( /[0-9]{2}([0-9]{2})([0-9]{2})([0-9]{2})/, function( match, year, month, day ) {
+                  return day +'.'+ month +'.'+ year
+                });
+                var gpstime = time.replace( /([0-9]{2})([0-9]{2})([0-9]{2})/, function( match, hour, minute, second ) {
+                  return hour +':'+ minute +':'+ second
+                });
+                var gpsTime = Moment.tz(gpsdate+" "+gpstime, "DD.MM.YY HH:mm:ss", "utc");
+                var datetime = gpsTime.format('YYYY-MM-DD HH:mm:ss')
+                result = {
+                  'raw': raw,
+                  'datetime': new Date().toISOString(),
+                //  'phone': str[1],
+                  'gps': {
+                    'date': gpsdate,
+                    'time': gpstime,
+                    //'signal': str[15] == 'F' ? 'full' : 'low',
+                    'fix': gpsData == 'A' ? 'active' : 'invalid'
+                  },
+                  'geo': {
+                    'latitude': tk102.fixGeo( latDegMin, latSec ),
+                    'longitude': tk102.fixGeo( lonDegMin, lonSec ),
+                    'speed': speed
+                    //'bearing': parseInt( str[10] )
+                  },
+                /*  'speed': {
+                    'knots': Math.round( str[9] * 1000 ) / 1000,
+                    'kmh': Math.round( str[9] * 1.852 * 1000 ) / 1000,
+                    'mph': Math.round( str[9] * 1.151 * 1000 ) / 1000
+                  },*/
+                  'imei': imei.trim()
+                }
+            } catch(e) {
+              //console.log("cannot parse data: "+raw+" / " +e);
+              return null;
+            }
         }
-    } catch(e) {
-      console.log("cannot parse data: "+raw+" / " +e);
-      return null;
     }
     return result
-  }
-
-  // 1203292316,0031698765432,GPRMC,211657.000,A,5213.0247,N,00516.7757,E,0.00,273.30,290312,,,A*62,F,imei:123456789012345,123
-  /*function(raw) {
-    var result = null
-    try {
+},
+// for TK108
+/*
+ 151207183420,+358456757390,GPRMC,103420.000,A,6629.0577,N,02543.2434,E,1.13,176.76,071215,,,A*67,F,,imei:867965023220144,05,86.5,F:3.93V/ADC1=0.64V/ADC2=0.00V,0,161,32821,244,36,238E,07ED,2.76km
+*/
+function(raw) {
+    //console.log("TK108");
+  var result = null
+  try {
       var raw = raw.trim()
-      var str = raw.split(',')
-
-      if( str.length == 18 && str[2] == 'GPRMC' ) {
-        var datetime = str[0].replace( /([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/, function( match, year, month, day, hour, minute ) {
-          return '20'+ year +'-'+ month +'-'+ day +' '+ hour +':'+ minute
-        })
-
-        var gpsdate = str[11].replace( /([0-9]{2})([0-9]{2})([0-9]{2})/, function( match, day, month, year ) {
-          return '20'+ year +'-'+ month +'-'+ day
-        })
-
-        var gpstime = str[3].replace( /([0-9]{2})([0-9]{2})([0-9]{2})\.([0-9]{3})/, function( match, hour, minute, second, ms ) {
-          return hour +':'+ minute +':'+ second +'.'+ ms
-        })
-
-        result = {
-          'raw': raw,
-          'datetime': datetime,
-          'phone': str[1],
-          'gps': {
-            'date': gpsdate,
-            'time': gpstime,
-            'signal': str[15] == 'F' ? 'full' : 'low',
-            'fix': str[4] == 'A' ? 'active' : 'invalid'
-          },
-          'geo': {
-            'latitude': tk102.fixGeo( str[5], str[6] ),
-            'longitude': tk102.fixGeo( str[7], str[8] ),
-            'bearing': parseInt( str[10] )
-          },
-          'speed': {
-            'knots': Math.round( str[9] * 1000 ) / 1000,
-            'kmh': Math.round( str[9] * 1.852 * 1000 ) / 1000,
-            'mph': Math.round( str[9] * 1.151 * 1000 ) / 1000
-          },
-          'imei': str[16].replace( 'imei:', '' )
-        }
+      var str = raw.split(",");
+      var imei = str[17].replace("imei:", "");
+      var dateS = str[11];
+      var time = str[3];
+      var gpsdate = dateS.replace( /([0-9]{2})([0-9]{2})([0-9]{2})/, function( match, day, month, year) {
+        return day +'.'+ month +'.'+ year
+      });
+      var gpstime = time.replace( /([0-9]{2})([0-9]{2})([0-9]{2})/, function( match, hour, minute, second ) {
+        return hour +':'+ minute +':'+ second
+      });
+      var gpsTime = Moment.tz(gpsdate+" "+gpstime, "DD.MM.YY HH:mm:ss", "utc");
+      var datetime = gpsTime.format('YYYY-MM-DD HH:mm:ss')
+      lat = str[5];
+      latSec = lat.split(".")[1];
+      latDegMin = lat.split(".")[0];
+      lon = str[7];
+      lonDegMin = lon.split(".")[0];
+      lonSec = lon.split(".")[1];
+      speed = str[9];
+      speed = parseFloat(speed);
+      result = {
+        'raw': raw,
+        'datetime': new Date().toISOString(),
+      //  'phone': str[1],
+        'gps': {
+          'dateTime': datetime,
+          //'signal': str[15] == 'F' ? 'full' : 'low',
+          'fix':'active'
+        },
+        'geo': {
+          'latitude': tk102.fixGeo( latDegMin, latSec ),
+          'longitude': tk102.fixGeo( lonDegMin, lonSec ),
+          'speed': Math.round( speed * 1.852 * 1000 ) / 1000
+          //'bearing': parseInt( str[10] )
+        },
+        'imei': imei
       }
-    } catch(e) {}
-    return result
-  }*/
+  } catch(e) {
+    console.log("cannot parse data tK108: "+raw+" / " +e);
+    return null;
+  }
+  return result
+}
+
 ]
 
 
@@ -181,6 +194,7 @@ tk102.createServer = function( vars ) {
 
     // receiving data
     socket.on( 'data', function( chunk ) {
+      console.log("data");
       console.log("chunk: "+chunk);
 
       var gps = null
@@ -209,9 +223,6 @@ tk102.createServer = function( vars ) {
       //console.log("closeConn: ");
       //var d = new Date();
       //console.log(d.toLocaleString() +": "+ data);
-
-
-
     })
 
     // error
@@ -242,17 +253,13 @@ tk102.createServer = function( vars ) {
 
 // Parse GPRMC string
 tk102.parse = function( raw ) {
-  var data = [];
-  var rawSplit = raw.split(')');
-  for(var i = 0, len = rawSplit.length; i<len; i++){
-        if(rawSplit[i] != ""){
-            d = specs[0]( rawSplit[i] );
-            if(d != null){
-                data.push(d);
-            }
+    var data = [];
+    for (var i = 0; i < specs.length; i++) {
+        d = specs[i]( raw );
+        if(d != null){
+            data.push(d);
         }
-    i++;
-  }
+    }
   return data;
 }
 
@@ -269,7 +276,6 @@ tk102.fixGeo = function( one, two ) {
   var res = degrees + (minutes / 60) ;
   //console.log(res);
   return res;
-
 }
 
 // ready
